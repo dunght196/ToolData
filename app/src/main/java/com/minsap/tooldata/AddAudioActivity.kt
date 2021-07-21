bf0ccf962efa393e5b35c2e34e48e705a8542d9a
dunght <dunght196@gmail.com>
2021-07-21 18:31:32 +0700

    add timber

--- app/src/main/java/com/minsap/tooldata/AddAudioActivity.kt
+++ app/src/main/java/com/minsap/tooldata/AddAudioActivity.kt
@@ -2,10 +2,34 @@ package com.minsap.tooldata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import com.minsap.tooldata.databinding.ActivityAddAudioBinding
+import com.minsap.tooldata.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class AddAudioActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddAudioBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_audio)
+        binding = ActivityAddAudioBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val db = FirebaseFirestore.getInstance()
+
+        val city = hashMapOf(
+            "name" to "Los Angeles",
+            "state" to "CA",
+            "country" to "USA"
+        )
+
+        db.collection("audio").document().set(city)
+            .addOnSuccessListener {
+                Timber.d("DocumentSnapshot successfully written")
+            }
+            .addOnFailureListener {
+                Timber.d("Error: $it")
+            }
     }
 }
